392b9187d9eb4888813ac48551b9dbe4fea9c906
Imperator-chebupel <138660337+Imperator-chebupel@users.noreply.github.com>
2024-05-22 23:15:10 +0700

    ////

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Моделирование_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(12, 235);
+            this.chart1.Name = "chart1";
+            series5.BorderWidth = 3;
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series2";
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(518, 528);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart2.Legends.Add(legend4);
+            this.chart2.Location = new System.Drawing.Point(551, 236);
+            this.chart2.Name = "chart2";
+            series7.BorderWidth = 3;
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series7.LabelBorderWidth = 3;
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            series8.BorderWidth = 3;
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series8.Legend = "Legend1";
+            series8.Name = "Series2";
+            this.chart2.Series.Add(series7);
+            this.chart2.Series.Add(series8);
+            this.chart2.Size = new System.Drawing.Size(518, 527);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(219, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(219, 127);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(219, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Объём выборок:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(219, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Интервалы:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(354, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Контрольная величина:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(354, 127);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(291, 96);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1108, 775);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Моделирование_1
+{
+    public partial class Form1 : Form
+    {
+
+        public double[] massiv1;
+        public double[] massiv2;
+        public int razmer1, razmer2, intervals;
+        public int[] kolichestvo1;
+        public int[] kolichestvo2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            int i;
+            razmer1 = Int32.Parse(textBox1.Text);
+            intervals = Int32.Parse(textBox3.Text);
+            //double lambda = Double.Parse(textBox4.Text);
+            massiv1 = new double[razmer1];
+            kolichestvo1 = new int[intervals];
+            for (i = 0; i < intervals; i++)
+            {
+                kolichestvo1[i] = 0;
+            }
+            int j;
+            Random r = new Random();
+            double u;
+            for (i = 0; i < razmer1; i++)
+            {
+                u = r.NextDouble();
+                massiv1[i] =  2 - 2 * Math.Sqrt(1.0 - u);//(u-1) / -0.5f; //(-Math.Log(u) / lambda);
+            }
+
+            double promezhutok1 = (massiv1.Max() - massiv1.Min()) / intervals;
+            for (i = 0; i < razmer1; i++)//расчёт попаданий в промежуток
+            {
+                for (j = 0; j < intervals; j++)
+                {
+
+                    if ((massiv1[i] < ((promezhutok1) * (j + 1))) && (massiv1[i] >= ((promezhutok1) * (j))))
+                    {
+                        kolichestvo1[j]++;
+                    }
+                }
+            }
+
+            double granica1 = massiv1.Min();//, granica2 = massiv2.Min();
+            int Y = 0;
+            Array.Sort(massiv1);
+            this.chart1.Series[0].Points.Clear();
+            this.chart1.Series[1].Points.Clear();
+            this.chart2.Series[0].Points.Clear();
+            this.chart2.Series[1].Points.Clear();
+            while (Y < intervals)
+            {
+                this.chart1.Series[0].Points.AddXY((granica1 + promezhutok1) / 2.0f, (Convert.ToDouble(kolichestvo1[Y])) / razmer1 / promezhutok1);
+                granica1 = granica1 + promezhutok1;
+                Y++;
+            }
+
+            granica1 = massiv1.Min();
+            Y = 0;
+            this.chart2.Series[0].Points.Clear();
+            double Xi = 0.0;
+            while (Y < intervals)
+            {
+                this.chart2.Series[0].Points.AddXY((granica1 + promezhutok1) / 2.0f, Convert.ToDouble(Summm(kolichestvo1, Y)) / razmer1);
+                this.chart2.Series[1].Points.AddXY((granica1 + promezhutok1) / 2.0f, Func(granica1, 0));
+                granica1 = granica1 + promezhutok1;
+
+                if (Math.Abs(Convert.ToDouble(Summm(kolichestvo1, Y)) / razmer1 - Func(granica1, 0)) > Xi)
+                    Xi = Math.Abs(Convert.ToDouble(Summm(kolichestvo1, Y)) / razmer1 - Func(granica1, 0));
+                Y++;
+            }
+            richTextBox1.Text += Xi;
+        }
+
+        public static int Summm(int[] kol, int stop)//накопительная сумма
+        {
+            int sum = 0;
+            for (int i = 0; i < stop + 1; i++)
+            {
+                sum += kol[i];
+            }
+            return sum;
+        }
+
+        public static double Func(double X, double lambda)
+        {
+            Random r = new Random();
+            double u = r.NextDouble();
+            return -X*X/4.0 + X;//1.0 - Math.Pow(Math.E, 0-lambda*X);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
